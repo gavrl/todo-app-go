49ce429f67e82394a3b69900be8b7dac64028390
sergeygavr <sergeygavr.94@yandex.ru>
2020-10-11 00:17:22 +0300

    Базовая структура проекта. HTTP-Сервер:   - создание структуры Сервер   - создание сущностей для работы с todo-app   - реализация обработчика rest-api запросов   - запуск сервера с обработчиком

---
+++ cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gavrl/todo-app"
+	"github.com/gavrl/todo-app/pkg/handler"
+	"log"
+)
+
+func main() {
+	handlers := new(handler.Handler)
+
+	srv := new(todo.Server)
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}

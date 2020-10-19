b9cf5235dd5b95c4575979b06572cd84f2ad0cce
sergeygavr <sergeygavr.94@yandex.ru>
2020-10-19 21:29:00 +0300

    Добавление config-файла, его инициализация библиотека для работы с конфигами: go get -u github.com/spf13/viper

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,28 @@ import (
 	"github.com/gavrl/todo-app/pkg/handler"
 	"github.com/gavrl/todo-app/pkg/repository"
 	"github.com/gavrl/todo-app/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }

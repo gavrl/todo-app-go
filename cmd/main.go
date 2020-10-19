1fb4f20e84e41e9b34d77697c42159aa718649cf
sergeygavr <sergeygavr.94@yandex.ru>
2020-10-19 20:53:28 +0300

    Создание репозитория,сервиса, обработчика Внедрение зависимостей

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"github.com/gavrl/todo-app"
 	"github.com/gavrl/todo-app/pkg/handler"
+	"github.com/gavrl/todo-app/pkg/repository"
+	"github.com/gavrl/todo-app/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {

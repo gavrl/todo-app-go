3a338fb0bfc11e7486ee3e82353298a5331e877f
sergeygavr <sergeygavr.94@yandex.ru>
2020-10-20 22:19:28 +0300

    Регистрация пользователей

--- pkg/repository/postgres.go
+++ pkg/repository/postgres.go
@@ -5,6 +5,14 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+const (
+	userTable       = "users"
+	todoListTable   = "todo_lists"
+	userListsTable  = "user_lists"
+	todoItemsTable  = "todo_items"
+	listsItemsTable = "lists_items"
+)
+
 type Config struct {
 	Host     string
 	Port     string
--- pkg/service/service.go
+++ pkg/service/service.go
@@ -1,8 +1,12 @@
 package service
 
-import "github.com/gavrl/todo-app/pkg/repository"
+import (
+	"github.com/gavrl/todo-app"
+	"github.com/gavrl/todo-app/pkg/repository"
+)
 
 type Authorization interface {
+	CreateUser(user todo.User) (int, error)
 }
 
 type TodoList interface {
@@ -18,5 +22,7 @@ type Service struct {
 }
 
 func NewService(repo *repository.Repository) *Service {
-	return &Service{}
+	return &Service{
+		Authorization: NewAuthService(repo.Authorization),
+	}
 }

46b4492be6b6f966262e60f0e74f2a59806735a2
SergeyGavr <sergeygavr.94@yandex.ru>
2021-09-19 12:54:53 +0300

    Обновление и удаление списков

--- pkg/service/todo_list.go
+++ pkg/service/todo_list.go
@@ -24,3 +24,15 @@ func NewTodoListService(repo repository.TodoList) *TodoListService {
 func (s *TodoListService) Create(userId int, list todo.TodoList) (int, error) {
 	return s.repo.Create(userId, list)
 }
+
+func (s *TodoListService) Delete(userId int, listId int) error {
+	return s.repo.Delete(userId, listId)
+}
+
+func (s *TodoListService) Update(userId int, listId int, input todo.UpdateListInput) error {
+	if err := input.Validate(); err != nil {
+		return err
+	}
+
+	return s.repo.Update(userId, listId, input)
+}
